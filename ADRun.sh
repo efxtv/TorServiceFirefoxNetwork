735d9c775ac4adb503961ca158b5f1095278df51
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2021-01-29 19:14:02 +0530

    Create ADRun.sh

---
+++ ADRun.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+echo "127.0.0.1:9050"
+service tor start
+echo "Please wait....."
+sleep 10
+lightgreen='\e[1;32m'
+echo -e "$lightgreen" ""
+while true; do
+sudo service tor restart  
+sleep 1
+curl --socks5 127.0.0.1:9050 https://check.torproject.org |& grep -Po "(?<=strong>)[\d\.]+(?=</strong)"|sed 's/^/IP Changed with────────────────────█──<><><>───[ /g'|sed 's/$/ ]/g'
+done
