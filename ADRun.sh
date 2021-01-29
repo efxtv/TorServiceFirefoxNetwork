af0cb24c7c6b3557b7b6b13ce136acede750d5f2
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2021-01-29 19:20:36 +0530

    Update ADRun.sh

--- ADRun.sh
+++ ADRun.sh
@@ -7,6 +7,6 @@ lightgreen='\e[1;32m'
 echo -e "$lightgreen" ""
 while true; do
 service tor reload  
-sleep 1
+sleep 15
 curl --socks5 127.0.0.1:9050 https://check.torproject.org |& grep -Po "(?<=strong>)[\d\.]+(?=</strong)"|sed 's/^/IP Changed with────────────────────█──<><><>───[ /g'|sed 's/$/ ]/g'
 done
